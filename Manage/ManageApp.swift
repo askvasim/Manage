519ccefbaa5b18a834370a5e8b158d88214ba749
Vasim Khan <75602156+askvasim@users.noreply.github.com>
2024-10-22 16:07:29 +0530

    Initial Commit

---
+++ Manage/ManageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManageApp.swift
+//  Manage
+//
+//  Created by Vasim Khan on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ManageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
